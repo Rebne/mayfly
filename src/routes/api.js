977f4364c0d4a1ce5b7dd7688284c376d9a45b2c
Rebne <127527467+Rebne@users.noreply.github.com>
2024-11-04 07:18:04 +0000

    Added some more API endpoints for notes

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,25 +3,36 @@ import { getNotesAndRemoveOldDB } from "../models/models.js";
 
 const router = express.Router();
 
-router.get("/notes/:id", async (req, res) => {
+router.get("/notes/:userId", async (req, res) => {
   try {
-    const userID = req.params.id;
+    const userID = req.params.userId;
     const notes = await getNotesAndRemoveOldDB(userID, req.app.locals.pool);
     res.status(200).send(notes.map((note) => note.content).join(", "));
   } catch (error) {
-    console.error("Error getting notes", error);
+    console.error("Error getting notes or deleting old notes", error);
     res.status(500).send("Internal server error");
   }
 });
 
-router.post("/notes", async (req, res) => {});
-
-router.delete("/messages/:id", async (req, res) => {
+router.post("/notes/:userId", async (req, res) => {
   try {
-    const userID = req.params.id;
-    deleteNote();
+    const { content } = req.body;
+    const userID = req.params.userId;
+    const newNote = await insertNoteDB(content, userID, req.app.locals.pool);
+    res.status(201).send(newNote);
   } catch (error) {
-    res.code(500);
+    console.error("Error inserting new note", error);
+    res.status(500).send("Internal server error");
+  }
+});
+
+router.delete("/notes/:noteId", async (req, res) => {
+  try {
+    const noteID = req.params.noteId;
+    await deleteNote(noteID, req.app.locals.pool);
+  } catch (error) {
+    console.error("Error deleting note", error);
+    res.code(500).send("Internal server error");
   }
 });
 
