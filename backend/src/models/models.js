18a5cd0ac5092174bcf81c80d345154076486061
Rebne <127527467+Rebne@users.noreply.github.com>
2024-11-06 09:27:11 +0000

    Added handlers for refresh token, register and added some correlating models

--- backend/src/models/models.js
+++ backend/src/models/models.js
@@ -1,8 +1,16 @@
-export const getUserIdentificationDB = async (username, password, pool) => {
+export const insertUserDB(username, password, pool) => {
+  const client = await pool.connect();
+  const res = await client.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *', [username, password])
+  console.log(res);
+  client.release();
+  return res;
+}
+
+export const getUserInfoDB = async (username, pool) => {
   const client = await pool.connect();
   const res = await client.query(
-    'SELECT id FROM users WHERE username = $1 AND password = $2',
-    [username, password],
+    'SELECT id, username, password FROM users WHERE username = $1',
+    [username],
   );
   console.log(res.rows[0]);
   client.release();
--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,4 +1,7 @@
-import express from 'express';
+import express, { json } from 'express';
+import crypto from 'crypto';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -6,16 +9,34 @@ const secret_key = process.env.SECRET_KEY;
 
 router.post('/login', loginHandler);
 router.post('/refresh-token', refreshHandler);
-router.post('/login', logoutHandler);
+router.post('/logout', logoutHandler);
+router.post('/register', registerHandler);
+
+router.post()
+
+const registerHandler = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await getUserInfoDB(username, req.app.locals.pool);
+    if (user) {
+      return res.status(409).json({ error: 'Username already exists' });
+    }
+    const newUser = await insertUserDB(username, hashedPassword, req.app.locals.pool);
+    const token = generateToken(newUser);
+    res.status(201).json({ token });
+  } catch (error) {
+    console.error('Error registering user', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
+
 
 const loginHandler = async (req, res) => {
   const { username, password } = req.body;
-  const user = await getUserIdentificationDB(
-    username,
-    password,
-    req.app.locals.pool,
-  );
-  if (!user)
+  const user = await getUserInfoDB(username, req.app.locals.pool);
+  if (!user || !bcrypt.compareSync(password, user.password))
     return res.status(401).json({ error: 'Invalid username or password' });
   const accessToken = jwt.sign({ id: user.id }, secret_key, {
     expiresIn: '15m',
@@ -31,12 +52,31 @@ const loginHandler = async (req, res) => {
 
 const refreshHandler = async (req, res) => {
   const refreshToken = req.body.refreshToken;
-  // Need hashing! Also for passwords
-  // Verify the refresh token
-  // Check the refresh token in DB, update and delete if expired
-  // Generate new access token
-  // Generate a new refresh token
-  
-}
+  if (!refreshToken) return res.code(400).json({ error: 'No refresh token' });
+  const decodedRefreshToken = jwt.verify(
+    refreshToken,
+    secret_key,
+    (err, decode) => {
+      if (err) return res.code(400).json({ error: 'Invalid refresh token' });
+      return decode;
+    },
+  );
+  const hash = crypto.createHash('sha256').update(refreshToken).digest('hex');
+  try {
+    await updateRefreshTokenByHashDB(hash, req.app.locals.pool);
+  } catch (error) {
+    return res.code(400).json({ error: 'Invalid refresh token' });
+  }
+  const newAccessToken = jwt.sign({ id: decodedRefreshToken.id }, secret_key, {
+    expiresIn: '15m',
+  });
+  const newRefreshToken = jwt.sign({ id: decodedRefreshToken.id }, secret_key, {
+    expiresIn: '30d',
+  });
+  return res.status(200).json({
+    accessToken: newAccessToken,
+    refreshToken: newRefreshToken,
+  });
+};
 
 export default router;
