9346abc84dd9fa681b3cf88e3e0134d75f0fefec
Rebne <renehgren@gmail.com>
2024-11-08 16:53:12 +0200

    Returning notes from get enpoint in desc order

--- backend/src/models/models.js
+++ backend/src/models/models.js
@@ -8,7 +8,7 @@ export const storeRefreshTokenDB = async (userID, hash, pool) => {
   const client = await pool.connect();
   const res = await client.query(
     "INSERT INTO tokens (user_id, hash, expires_at) VALUES($1, $2, NOW() + INTERVAL '30 days')",
-    [userID, hash],
+    [userID, hash]
   );
   console.log(res.rows[0]);
   client.release();
@@ -18,7 +18,7 @@ export const updateRefreshTokenDB = async (userID, hash, pool) => {
   const client = await pool.connect();
   const res = await client.query(
     "UPDATE tokens SET hash = $1, expires_at = NOW() + INTERVAL '30 days' WHERE user_id = $3",
-    [hash, userID],
+    [hash, userID]
   );
   client.release();
   return res.rows[0];
@@ -28,7 +28,7 @@ export const storeUserDB = async (username, password, pool) => {
   const client = await pool.connect();
   const res = await client.query(
     'INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *',
-    [username, password],
+    [username, password]
   );
   console.log(res.rows[0]);
   client.release();
@@ -39,7 +39,7 @@ export const getUserInfoDB = async (username, pool) => {
   const client = await pool.connect();
   const res = await client.query(
     'SELECT id, username, password FROM users WHERE username = $1',
-    [username],
+    [username]
   );
   console.log(res.rows[0]);
   client.release();
@@ -50,7 +50,7 @@ export const storeNoteDB = async (content, userID, pool) => {
   const client = await pool.connect();
   const res = await client.query(
     'INSERT INTO notes (content, user_id) VALUES ($1, $2) RETURNING *',
-    [content, userID],
+    [content, userID]
   );
   console.log(res.rows[0]);
   client.release();
@@ -64,10 +64,11 @@ export const deleteNoteDB = async (noteID, pool) => {
 
 export const getNotesAndRemoveOldDB = async (username, pool) => {
   const client = await pool.connect();
-  const res = await client.query('SELECT * FROM notes WHERE user_id = $1', [
-    username,
-  ]);
-  const twelveHoursAgo = new Date(Date.now() - 1000);
+  const res = await client.query(
+    'SELECT * FROM notes WHERE user_id = $1 ORDER BY id DESC',
+    [username]
+  );
+  const twelveHoursAgo = new Date(Date.now() - 1000); // FOR TESTING
   // const twelveHoursAgo = new Date(Date.now() - 1000 * 60 * 60 * 12);
   const rowsToDeleteIDs = res.rows
     .filter((row) => row.created_at <= twelveHoursAgo)
